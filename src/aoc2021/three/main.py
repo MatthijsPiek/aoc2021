87d9063ee701e9b9abcb0224224a8483f4e2b0cd
Matthijs Piek <matthijs@sandcat.nl>
2021-12-04 09:26:49 +0100

    Refactor into method object

--- src/aoc2021/three/main.py
+++ src/aoc2021/three/main.py
@@ -1,71 +1,72 @@
 from typing import List, Dict
 
-def count_characters_per_column(input: str) -> List[Dict]:
-    # Split the input string into a list of lines
-    lines = input.splitlines()
-    # Get the number of columns
-    columns = len(lines[0].strip())
-    # Initialize a list to hold the results
-    results = [{} for i in range(columns)]
-    # Iterate over the lines
-    for line in lines:
-        # Split the line into characters
-        characters = line.strip()
-        # Iterate over the characters
-        for i in range(len(characters)):
-            # Get the character
-            character = characters[i]
-            # Get the column number
-            column = i
-            # If the character is not in the results list, add it
-            if character not in results[column]:
-                results[column][character] = 1
-            # Otherwise, increment the count
-            else:
-                results[column][character] += 1
-    # Return the results
-    return results
 
-def most_common_character(column: Dict[str, int]) -> str:
-    # Get the character with the highest count
-    character, count = max(column.items(), key=lambda x: x[1])
-    # Return the character
-    return character
-
-
-def least_common_character(column: Dict[str, int]) -> str:
-    # Get the character with the lowest count
-    character, count = min(column.items(), key=lambda x: x[1])
-    # Return the character
-    return character
-
-
-def most_common_characters(input: str):
-    char_count_per_column = count_characters_per_column(input)
+class CountPerColumn:
+    def __init__(self, lines: List[str]) -> None:
+        self.lines = lines
+        self.columns = len(self.lines[0].strip())
+        self.results = []
+        self.count_characters_per_column()
     
-    output = ""
-    for column in char_count_per_column:
-        output += most_common_character(column)
-        
-    return output
+    def count_characters_per_column(self) -> None:
+        # Initialize a list to hold the results
+        self.results = [{} for i in range(self.columns)]
+        # Iterate over the lines
+        for line in self.lines:
+            characters = line.strip()
+            # Iterate over the characters
+            for i in range(len(characters)):
+                # Get the character
+                character = characters[i]
+                # Get the column number
+                column = i
+                # If the character is not in the results list, add it
+                if character not in self.results[column]:
+                    self.results[column][character] = 1
+                # Otherwise, increment the count
+                else:
+                    self.results[column][character] += 1
+                    
+    def most_common_character(self, column: Dict[str, int]) -> str:
+        # Get the character with the highest count
+        character, count = max(column.items(), key=lambda x: x[1])
 
-def least_common_characters(input: str):
-    char_count_per_column = count_characters_per_column(input)
-    
-    output = ""
-    for column in char_count_per_column:
-        output += least_common_character(column)
+        return character
+
+    def least_common_character(self, column: Dict[str, int]) -> str:
+        # Get the character with the lowest count
+        character, count = min(column.items(), key=lambda x: x[1])
         
-    return output
+        return character
+
+
+    def most_common_characters(self) -> str:        
+        output = ""
+        for column in self.results:
+            output += self.most_common_character(column)
+            
+        return output
+
+    def least_common_characters(self) -> str:
+        output = ""
+        for column in self.results:
+            output += self.least_common_character(column)
+            
+        return output
 
 def binary_str_to_int(input: str) -> int:
     return int(input, 2)
 
+def oxygen_generator_rating(input: str) -> int:
+    pass    
+    
 if __name__ == "__main__":
     with open('src/aoc2021/three/input', 'r') as f:
         data = f.read()
         
-        gamma_rate = binary_str_to_int(most_common_characters(data))
-        epsilon_rate = binary_str_to_int(least_common_characters(data))
+        count = CountPerColumn(data.splitlines())
+        
+        gamma_rate = binary_str_to_int(count.most_common_characters(data))
+        epsilon_rate = binary_str_to_int(count.least_common_characters(data))
         
         print(f"Answer: {gamma_rate * epsilon_rate}")
--- tests/three/test_three.py
+++ tests/three/test_three.py
@@ -1,7 +1,7 @@
-from aoc2021.three.main import most_common_characters, least_common_characters, binary_str_to_int
+from aoc2021.three.main import CountPerColumn, binary_str_to_int, oxygen_generator_rating
 
-def test_example_part_one():
-    input = \
+
+EXAMPLE_INPUT = \
         """00100
 11110
 10110
@@ -14,12 +14,18 @@ def test_example_part_one():
 11001
 00010
 01010
-"""
-    assert most_common_characters(input) == '10110'
-    assert least_common_characters(input) == '01001'
+""".splitlines()
+
+def test_example_part_one():
+    count = CountPerColumn(EXAMPLE_INPUT)
+    assert count.most_common_characters() == '10110'
+    assert count.least_common_characters() == '01001'
     
 def test_binary_conversion():
     assert binary_str_to_int('10110') == 22
     assert binary_str_to_int('01001') == 9
+    
+def test_example_part_two():
+    assert oxygen_generator_rating(EXAMPLE_INPUT) == "10111"
     
     
